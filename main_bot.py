2a52a4a55c2adf245bf533bd119890cf6cbf1064
Bayazit <bakhasanov@gmail.com>
2022-07-22 01:33:36 +0300

    Created main markup

--- main_bot.py
+++ main_bot.py
@@ -1,19 +1,27 @@
 from config import TOKEN
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 # инициализация бота
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-# клавиатуры
-markup_main = InlineKeyboardMarkup().add(InlineKeyboardButton('Добавить предмет', callback_data='add_object'))
-
 # старт
+tasks_button = KeyboardButton('📋 Задачи')
+add_task = KeyboardButton('✏ Добавить задачу')
+
+markup_main = ReplyKeyboardMarkup(resize_keyboard=True)
+markup_main.add(tasks_button).add(add_task)
+
 @dp.message_handler(commands="start")
 async def start_command(message : types.Message):
-	await message.answer('Предметы:', reply_markup=markup_main)
+	await message.answer('Привет! Вот что я умею:', reply_markup=markup_main)
+
+# список задач
+
+@dp.message_handler(lambda message: message.text == "📋 Задачи")
+async def tasks_command(message : types.Message):
+	await message.answer('Задачи:')
 
 # поллинг
 if __name__ == '__main__':
